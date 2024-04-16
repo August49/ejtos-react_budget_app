81e01fbdd22cb83fcaef2b53ab90be114c1269f5
august49 <augustiniusngongo@gmail.com>
2024-04-16 23:21:26 +0530

    initial commit

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -24,6 +24,18 @@ const ExpenseItem = (props) => {
     });
   };
 
+  const decreaseAllocation = (name) => {
+    const expense = {
+      name: name,
+      cost: 10,
+    };
+
+    dispatch({
+      type: "RED_EXPENSE",
+      payload: expense,
+    });
+  };
+
   return (
     <tr>
       <td>{props.name}</td>
@@ -33,6 +45,9 @@ const ExpenseItem = (props) => {
       </td>
       <td>
         <button onClick={(event) => increaseAllocation(props.name)}>+</button>
+      </td>
+      <td>
+        <button onClick={(event) => decreaseAllocation(props.name)}>-</button>
       </td>
       <td>
         <TiDelete size="1.5em" onClick={handleDeleteExpense}></TiDelete>
